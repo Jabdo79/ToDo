d804f620cea25686dee9fa530664445c91e9696c
Jonathan Abdo <jabdo79@gmail.com>
2016-08-12 15:31:39 -0400

    Branch for JavaFX, experimenting

---
+++ src/main/java/abdo/jonathan/ToDoGui.java
@@ -0,0 +1,18 @@
+package abdo.jonathan;/**
+ * Created by Jonathan on 8/12/2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ToDoGui extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
